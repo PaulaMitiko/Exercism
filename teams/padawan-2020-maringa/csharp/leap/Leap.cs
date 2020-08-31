571a6518c5150a081862cb74355523eb15e99d55
PaulaMitiko <paula.heller@hbsis.com.br>
2020-08-31 09:21:48 -0300

    Exercícios

--- teams/padawan-2020-maringa/csharp/leap/Leap.cs
+++ teams/padawan-2020-maringa/csharp/leap/Leap.cs
@@ -4,6 +4,8 @@ public static class Leap
 {
     public static bool IsLeapYear(int year)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        bool confirmation = false;
+        if (year % 400 == 0 || year % 4 == 0 && year % 100 != 0) confirmation = true;
+        return confirmation;
     }
 }
