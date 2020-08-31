95977e3241e4e5405d062f4bb94114be20c57f14
PaulaMitiko <paula.heller@hbsis.com.br>
2020-08-31 08:56:42 -0300

    Two Fer

---
+++ teams/padawan-2020-maringa/csharp/two-fer/TwoFer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+public static class TwoFer
+{
+    public static string Speak()
+    {
+        string frase = "One for you, one for me.";
+        return frase;
+    }
+    public static string Speak(string who)
+    {      
+        string frase = "One for " + who + ", one for me.";
+        return frase;
+    }
+}
